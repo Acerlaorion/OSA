8096e6b81ff6321d39436d4b700efcb4b8dc3196
Acerlaorion <52343449+Acerlaorion@users.noreply.github.com>
2019-08-30 20:24:22 +0300

    LAST VERSION

--- main.cpp
+++ main.cpp
@@ -2,14 +2,15 @@
 #include <SFML/Audio/Music.hpp>
 #include <SFML/Audio/Listener.hpp>
 #include <SFML/Audio.hpp>
-#include <time.h>
 #include <list>
-#include "menu.h"
 #include <string>
+#include "menu.h"
 using namespace sf;
 
 int main()
 {
-    RenderWindow window(sf::VideoMode(1920, 1080), "OSA Pre-Alpha");
+    RenderWindow window;
+    window.create(sf::VideoMode(1920, 1080), "OSA!", sf::Style::Fullscreen);//создание окна
     menu(window);
+    return 0;
 }
